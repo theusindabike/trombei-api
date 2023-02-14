67f160153749f30f29cd54cab477d2f2a1c6d7ae
Matheus Lopes <mlopes.matheus@gmail.com>
2023-02-14 17:06:53 -0300

    chore: fix tests

--- trombei_api/places/tests/test_places_api.py
+++ trombei_api/places/tests/test_places_api.py
@@ -3,7 +3,7 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase, APIClient
 
-from trombei_api.places.models import Place
+from trombei_api.places.models import Place, DirectionUrl
 
 PLACE_CREATE_AND_LIST_URL = reverse("places:place-list", kwargs={"version": "v1"})
 
@@ -86,7 +86,7 @@ class PlaceAPITest(APITestCase):
 
     def test_direction_urls(self):
         """
-        Ensure we can add mutiples directions urls
+        Ensure we can add mutiples direction urls
         """
 
         self.client.force_authenticate(user=self.user_1)
@@ -109,3 +109,86 @@ class PlaceAPITest(APITestCase):
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response.data.get("direction_urls"), data["direction_urls"])
+
+    def test_add_direction_urls_to_an_existing_place(self):
+        """
+        Ensure we can add directions urls to a existing place
+        """
+
+        place = Place.objects.get(id="00000000-0000-0000-0000-000000000001")
+
+        self.client.force_authenticate(user=self.user_1)
+
+        url = reverse("places:place-detail", kwargs={"version": "v1", "pk": place.id})
+
+        existing_place = self.client.get(url).data
+
+        direction_urls = [
+            {
+                "type": "GOOGLE-MAPS",
+                "url": "https://www.google.com/x",
+            },
+            {
+                "type": "UBER",
+                "url": "https://m.uber.com/ul/y",
+            },
+        ]
+
+        existing_place["direction_urls"] = direction_urls
+
+        url = reverse("places:place-detail", kwargs={"version": "v1", "pk": place.id})
+
+        response = self.client.put(url, data=existing_place)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data.get("id"), str(place.id))
+        self.assertEqual(response.data.get("direction_urls"), direction_urls)
+
+    def test_update_direction_urls_and_place(self):
+        """
+        Ensure we can update directions urls and place
+        """
+
+        self.client.force_authenticate(user=self.user_1)
+
+        place_with_directions = {
+            "name": "Place Name 1",
+            "full_address": "Full Address 1",
+            "direction_urls": [
+                {
+                    "type": DirectionUrl.DirectionType.GOOGLE_MAPS,
+                    "url": "https://www.google.com/y",
+                }
+            ],
+        }
+        response = self.client.post(
+            PLACE_CREATE_AND_LIST_URL, data=place_with_directions
+        )
+        existing_place = response.data
+
+        updated_place_name = "Place Name 2"
+        updated_direction_urls = [
+            {
+                "type": DirectionUrl.DirectionType.GOOGLE_MAPS,
+                "url": "https://www.google.com/y",
+            },
+            {
+                "type": DirectionUrl.DirectionType.UBER,
+                "url": "https://www.uber.com/x",
+            },
+        ]
+
+        existing_place["direction_urls"] = updated_direction_urls
+        existing_place["name"] = updated_place_name
+
+        url = reverse(
+            "places:place-detail",
+            kwargs={"version": "v1", "pk": existing_place.get("id")},
+        )
+
+        response = self.client.put(url, data=existing_place)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data.get("id"), str(existing_place.get("id")))
+        self.assertEqual(response.data.get("name"), updated_place_name)
+        self.assertEqual(len(response.data.get("direction_urls")), 3)
